8f313bdf7f5173faf7dcb49ca81d4cf02d7417ec
Elisa <elisa.lazar@nhs.net>
2023-10-05 19:01:04 +0100

    changed screen content, to change routing, country

--- app/assets/javascript/main.js
+++ app/assets/javascript/main.js
@@ -1,1 +1,13 @@
 // ES6 or Vanilla JavaScript
+
+if(document.querySelector('#eu-location-picker')) {
+    accessibleAutocomplete.enhanceSelectElement({
+      selectElement: document.querySelector('#eu-location-picker')
+    })
+}
+
+if(document.querySelector('#eu-efta-location-picker')) {
+    accessibleAutocomplete.enhanceSelectElement({
+      selectElement: document.querySelector('#eu-uk-location-picker')
+    })
+}
