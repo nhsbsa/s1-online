32f940c57e7b4276858d5e607bcda8151a5ba31c
Elisa <elisa.lazar@nhs.net>
2023-12-06 17:15:48 +0000

    added email validation making field mandatory

--- app/views/mvp/_routes.js
+++ app/views/mvp/_routes.js
@@ -1212,16 +1212,16 @@ router.post([/applicant-correspondence-address/, /applicant-correspondence-addre
 
 router.post([/applicant-email/, /applicant-email-error/], function (req,res) {
     
-    console.log(req.body.email);
+    var email = req.session.data['email'];
 
     const emailRegEx = /^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$/;
   
-    if(req.body.email !== '' && emailRegEx.test(req.body.email)) {
+    if(email != '' && emailRegEx.test(email)) {
       res.redirect('applicant-phone');
-    } else if(req.body.email !== '' && !emailRegEx.test(req.body.email)) {
+    } else if(email == '') {
       res.redirect('applicant-email-error');
-    } else {
-      res.redirect('applicant-phone');
+    } else if(email != '' && !emailRegEx.test(email)) {
+        res.redirect('applicant-email-error');
     }
   })
 
