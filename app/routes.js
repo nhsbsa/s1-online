90c55d9bf83e66d3d930ff74267daeb2fb5329c6
nhsbsa-drobs <darren.robson@nhsbsa.nhs.uk>
2024-02-14 10:33:02 +0000

    Routes fix

--- app/routes.js
+++ app/routes.js
@@ -9,8 +9,8 @@ const router = express.Router();
 
 router.use('/mvp', require('./views/mvp/_routes'));
 router.use('/mvp/file-upload', require('./views/mvp/file-upload/_routes'));
-router.use('/mvp/download-succes', require('./views/mvp/download-success/_routes'));
-router.use('/mvp/download-unsucces', require('./views/mvp/download-unsuccess/_routes'));
+router.use('/mvp/download-success', require('./views/mvp/download-success/_routes'));
+router.use('/mvp/download-unsuccess', require('./views/mvp/download-unsuccess/_routes'));
 
 router.use('/discovery', require('./views/discovery/_routes'));
 
