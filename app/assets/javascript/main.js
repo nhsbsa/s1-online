32277f4de12fa4d191d7ace9d0f8250ab889ab24
nhsbsa-drobs <darren.robson@nhsbsa.nhs.uk>
2024-03-06 11:32:04 +0000

    Add country button below other inputs

--- app/assets/javascript/main.js
+++ app/assets/javascript/main.js
@@ -238,10 +238,9 @@ function removeInputEEA(btn){
 
   // Count current amount of inputs in section
   var inputCountOther = el.closest('div').getElementsByTagName('select').length;
-  console.log(inputCountOther);
 
   // Create ID for new select we're going to add
-  var newSelectID = `${el.previousElementSibling.className}-${inputCountOther}`;
+  var newSelectID = `${el.previousElementSibling.id}-${inputCountOther}`;
 
   let generatedOptionList = generateSelectList(spEuCountries);
 
@@ -250,6 +249,8 @@ function removeInputEEA(btn){
 
   // Use ID of new select to initilise autcomplete for our new select
   let select = document.querySelector(`#${newSelectID}`)
+
+  console.log(newSelectID);
 
   accessibleAutocomplete.enhanceSelectElement({
     selectElement: select,
