48a1e91abb62b5568a072e6c3ae8c745ea1fbab8
tarikjohnston1 <hi.tarik.johnston@nhsbsa.nhs.uk>
2024-03-26 15:15:39 +0000

    Update to email journey

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -2,6 +2,7 @@ module.exports = {
 
     // Insert values here
     'ineligibleTitle': 'You cannot apply for an S1 for healthcare cover',
+    'OHS': '25001275',
     'eligibilityCaption': 'Eligibility',
     'mainCaption': 'Main application',
     'familyCaption': 'Add family members',
