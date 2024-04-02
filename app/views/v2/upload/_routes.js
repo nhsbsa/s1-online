d525782b3fa08bafe42d4a0f26d1ff34ee1c4161
tarikjohnston1 <hi.tarik.johnston@nhsbsa.nhs.uk>
2024-04-02 09:27:56 +0100

    Update to multiple upload

--- app/views/v2/upload/_routes.js
+++ app/views/v2/upload/_routes.js
@@ -66,7 +66,7 @@ router.post([/dob/], function (req, res) {
 
     } else if(req.body.dateOfBirth !== '' ) {
         data.error = 'false';
-        res.redirect('have-evidence');
+        res.redirect('record-found');
     }   
 }) 
 
@@ -88,113 +88,13 @@ function getMonthInLetters(month) {
 }
 
 
-// Do you have the evidence we asked for? - evidence.html
-router.post([/have-evidence/], function (req, res) {
-    var evidence = req.session.data['evidence'];
-    const data = req.session.data;
-
-    if(evidence == 'Yes') {
-        data.error = 'false';
-        res.redirect('provide-eea-pension-evid');
-    } else if(evidence == 'No') {
-        data.error = 'false';
-        res.redirect('additional-info');
-    } else {
-        data.error = 'true';
-        res.redirect('have-evidence');
-    }
-})
- 
-// If you cannot provide the evidence we asked for, tell us why
-router.post([/additional-info/], function (req, res) {
-    var addInfo = req.session.data['addInfo'];
-    const data = req.session.data;
-
-    if (addInfo == '') {
-        data.error = 'true';
-        res.redirect('additional-info');
-    }
-    else if (addInfo != '') {
-        data.error = 'false';
-        res.redirect('check-your-answers');
-    }
+router.post([/record-found/], function (req,res) {
+    res.redirect('upload-evidence') 
 })
 
-
-
-
-// Have you been asked to give evidence for the years you paid pension contributions to the UK or to other countries?
-router.post([/provide-eea-pension-evid/], function (req, res) {
-    var provideEEAPensEvid = req.session.data['provideEEPensEvid'];
-    const data = req.session.data;
-
-    if(provideEEAPensEvid == 'Yes') {
-        data.error = 'false';
-        res.redirect('eea-countries');
-    } else if(provideEEAPensEvid == 'No') {
-        data.error = 'false';
-        res.redirect('provide-uk-pension-evid');
-    } else {
-        data.error = 'true';
-        res.redirect('provide-eea-pension-evid');
-    }
-})
-
-// // Which countries have you been asked to give evidence for, other than the UK?
-// router.post([/eea-countries/, /eea-countries-error/], function (req, res) {
-//     var eeaCountries = req.session.data['eeaCountries'];
-//     console.log(eeaCountries);
-
-//     if (eeaCountries == 'Iceland' || eeaCountries == 'Liechtenstein' || eeaCountries == 'Norway' || eeaCountries == 'Switzerland') {
-//         res.redirect('upload-eea-evidence');
-//     } else if (eeaCountries == 'Austria' || eeaCountries == 'Belgium' || eeaCountries == 'Bulgaria' || eeaCountries == 'Denmark') {
-//         res.redirect('upload-eea-evidence');
-//     } else if (eeaCountries == 'Czech Republic' || eeaCountries == 'Estonia' || eeaCountries == 'Finland' || eeaCountries == 'France') {
-//         res.redirect('upload-eea-evidence');
-//     } else if (eeaCountries == 'Germany' || eeaCountries == 'Greece' || eeaCountries == 'Hungary' || eeaCountries == 'Ireland' || eeaCountries == 'Italy') {
-//         res.redirect('upload-eea-evidence');
-//     } else if (eeaCountries == 'Latvia' || eeaCountries == 'Lithuania' || eeaCountries == 'Luxemburg' || eeaCountries == 'Malta' || eeaCountries == 'Montenegro') {
-//         res.redirect('upload-eea-evidence');
-//     } else if (eeaCountries == 'Netherlands' || eeaCountries == 'Poland' || eeaCountries == 'Portugal' || eeaCountries == 'Romania' || eeaCountries == 'Slovakia') {
-//         res.redirect('upload-eea-evidence');
-//     } else if (eeaCountries == 'Slovenia' || eeaCountries == 'Spain' || eeaCountries == 'Sweden') {
-//         res.redirect('upload-eea-evidence');
-//     } else if (eeaCountries == '') {
-//         res.redirect('eea-countries-error');
-//     } 
-// })
-
-// Have you been asked to give evidence that you paid a UK State Pension?
-router.post([/provide-uk-pension-evid/], function (req, res) {
-
-var provideUKPensEvid = req.session.data['provideUKPensEvid'];
-const data = req.session.data;
-
-if(provideUKPensEvid == 'Yes') {
-    data.error = 'false';
-    res.redirect('upload-uk-evidence');
-} else if(provideUKPensEvid == 'No') {
-    data.error = 'false';
-    res.redirect('check-your-answers');
-} else {
-    data.error = 'true';
-    res.redirect('provide-uk-pension-evid');
-}
-})
-
-// Upload evidence that you are being paid a UK State Pension
-router.post([/upload-uk-evidence/], function (req,res) {
+router.post([/upload-evidence/], function (req,res) {
     res.redirect('check-your-answers') 
 })
-// Upload evidence that you are being paid a UK State Pension
-router.post([/eea-countries/], function (req,res) {
-    res.redirect('upload-ee-evidence') 
-})
-// Upload evidence that you are being paid a UK State Pension
-router.post([/upload-ee-evidence/], function (req,res) {
-    res.redirect('check-your-answers') 
-})
-
 
 router.post([/check-your-answers/], function (req,res) {
     res.redirect('confirmation') 
